c704056049d226fdabf76321144c78f1ad7fb71d
Taira_Chyan8 <tairachyan@gmail.com>
2022-05-25 18:58:00 +0300

    Dialog, Its very simple

---
+++ blobsTime/Assets/csipts/DialogData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogData : MonoBehaviour
+{
+    [SerializeField] private string DialogName;
+    [SerializeField] private List<DialogStage> _dialogStages;
+
+    public bool GetDialogStage(int Position, out DialogStage dialogStage)
+    {
+        dialogStage = _dialogStages.Find(stage => stage.Position == Position);
+        if (dialogStage == null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+}
+[System.Serializable]
+public class DialogStage
+{
+    public int Position;
+    public string DialogText;
+    public string CharecterName;
+    public Sprite CharacterIcon;
+}
---
+++ blobsTime/Assets/csipts/SimpleDialog.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SimpleDialog : MonoBehaviour
+{
+    [SerializeField] private Image _characterImage;
+    [SerializeField] private TMP_Text _characterText;
+    [SerializeField] private TMP_Text _dialogText;
+    [SerializeField] private Button _nextStageButton;
+
+    [SerializeField] private DialogData _temporaryData;
+
+    private int position = -1;
+    private void NextDialog()
+    {
+        position++;
+        if (_temporaryData.GetDialogStage(position, out var dialog))
+        {
+            _characterImage.sprite = dialog.CharacterIcon;
+            _characterText.text = dialog.CharecterName;
+            _dialogText.text = dialog.DialogText;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        position = -1;
+        _nextStageButton.onClick.AddListener(NextDialog);
+        NextDialog();
+    }
+}
+
